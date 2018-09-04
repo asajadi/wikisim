4e196b67fd843152fcf2aad2f2443712d9fcf257
Armin Sajadi <sajadi@cs.dal.ca>
2018-09-04 10:11:58 -0300

    fixing documentation for reading the sql dumps

--- preparation_scripts/db/startimport.sh
+++ preparation_scripts/db/startimport.sh
@@ -1,5 +1,5 @@
 # This scripts creates a database and starts importing the tables
-# Usage: bash startimport.sh <dir> <vesrion> <user> <pass>
+# Usage: bash startimport.sh <directory of the dumps> <vesrion> <user> <pass>
 # Current version of the dumps is 20160305
 #%%system
 mysql -u $1 -p$2 -e 'CREATE SCHEMA `enwikilast` DEFAULT CHARACTER SET binary;'
